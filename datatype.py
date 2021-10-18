c9a1f14a04179809b16e9256a38a365316fccf9f
nemo-park <pklemontree@gmai.com>
2021-10-18 20:50:55 +0900

    Commit

---
+++ datatype.py
@@ -0,0 +1,22 @@
+# 숫자 자료형
+print(5)
+print(-10)
+print(3.14)
+print(920212)
+print(5+3)
+print(5*2)
+print(3*(3+1))
+
+# 문자열 자료형
+print("크리스마스")
+print('선물')
+print("*"*10)
+
+# Boolean 자료형
+print(5 > 10)
+print(5 < 10)
+print(True)
+print(False)
+print(not True)
+print(not False)
+print(not (5 > 10))
