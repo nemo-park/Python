b76d03ec314e5d5a00650e0eccbed0783b961616
nemo-park <pklemontree@gmail.com>
2021-10-20 22:24:12 +0900

    숫자처리함수

--- 3_function1.py
+++ 3_function1.py
@@ -1,13 +1,13 @@
-print(abs(-5)) #5
-print(pow(4, 2)) # 4^2 = 4*4 = 16
+print(abs(-5))     #5
+print(pow(4, 2))   # 4^2 = 4*4 = 16
 
-print(max(5, 12)) #12
-print(min(5, 12)) #5
+print(max(5, 12))  #12
+print(min(5, 12))  #5
 print(round(3.14)) #3
 print(round(4.99)) #5
 
 # math library 안의 모든것을 이용하겠다!
 from math import *
 print(floor(4.99)) # 내림 4
-print(ceil(3.14)) # 올림 4
-print(sqrt(16)) # 제곱근 4
+print(ceil(3.14))  # 올림 4
+print(sqrt(16))    # 제곱근 4
