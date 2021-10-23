de385554c9355e07a5c8e2798a13e930096f8603
nemo-park <pklemontree@gmail.com>
2021-10-23 22:43:49 +0900

    슬라이싱

--- 4_1 string.py
+++ 4_1 string.py
@@ -10,3 +10,18 @@ sentence3 = """
 Python은 꿀잼!
 """
 print(sentence3)
+
+juminNum = "920212-1234567"
+print("성별 : " + juminNum[7])
+print("Year : " + juminNum[0:2])
+print("Month : " + juminNum[2:4])
+print("Day : " + juminNum[4:6])
+print("Birth : " + juminNum[0:6])
+# 처음부터 6 직전까지 Print
+print("Birth : " + juminNum[:6])
+
+print("뒤 7자리 : " + juminNum[7:14])
+# 7부터 끝까지
+print("뒤 7자리 : " + juminNum[7:])
+# 맨 뒷자리는 -1, 맨 뒤에서 7번째부터 끝까지
+print("뒤 7자리 (뒤에서부터) : " + juminNum[-7:])
